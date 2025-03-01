557b291bd808f9b1283feecbedeeea1c790d500f
Dulneth Santhuka <dulneth.sa@gmail.com>
2025-03-01 09:44:40 +0530

    write sample in the file.txt

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
-fs.readFile("file.txt", "utf8", function (err, data) {
+/* fs.readFile("file.txt", "utf8", function (err, data) {
+   if (err) throw err;
+   console.log(data);
+ }); */
+
+fs.write("file.txt", "Hello World", function (err) {
   if (err) throw err;
-  console.log(data);
+  console.log("File saved");
 });
